87403c25fbead300bc1d057b985bf870066c9014
AlbertoDamaso <alberto.matheus@hotmail.com>
2022-12-23 18:20:20 -0300

    eveentoys-frontHeader

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from "./components/Header";
 import Home from "./screens/Home";
+import SignUp from "./screens/SignUp";
 
 function RoutesApp(){
     return(
@@ -8,6 +9,7 @@ function RoutesApp(){
             <Header/>
             <Routes>
                 <Route path="/" element={ <Home/> }/>
+                <Route path="/signup" element={ <SignUp/> }/>
             </Routes>
         </BrowserRouter>
     )
